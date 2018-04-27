f6f19ef94437ef720128bc1ede8e7146449c6cb5
monikakalt <kaltenytmonika3@gmail.com>
2018-04-27 19:32:46 +0300

    added controllers

---
+++ backend/challenge.Web/challenge.Application/automapper/MappingsProfile.cs
@@ -0,0 +1,24 @@
+﻿using akademij.Application.main.employee.dto;
+using akademij.Application.main.inventory;
+using akademij.EF.entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace akademij.Application.automapper
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            CreateMap<Employee, EmployeeDto>()
+                .ForMember(dto => dto.EmployeeInventory, opt => opt.MapFrom(x => x.EmployeeInventory.Select(y => y.Inventory).ToList()));
+            CreateMap<Inventory, InventoryViewDto>();
+           // CreateMap<Inventory, InventoryDto>().ReverseMap();
+           // CreateMap<InventoryType, InventoryTypeDto>();
+
+        }
+    }
+}
