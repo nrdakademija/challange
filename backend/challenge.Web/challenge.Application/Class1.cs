310b7cc466622037e59d685e9afad3a46f945cfd
monikakalt <kaltenytmonika3@gmail.com>
2018-04-27 18:48:17 +0300

    backend application innitialize

---
+++ backend/challenge.Web/challenge.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace challenge.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ backend/challenge.Web/challenge.EF/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace challenge.EF
+{
+    public class Class1
+    {
+    }
+}
